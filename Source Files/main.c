b5c4a8a2c329094e7fbd03b7a6c89f49bd01c619
devmichalek <devmichalek@gmail.com>
2019-06-14 16:44:34 +0200

    Update project settings.

--- Source Files/main.c
+++ Source Files/main.c
@@ -1,23 +1,6 @@
-#include "grayscale.h"
-#include "color_reduction.h"
-#include "error_diffusion.h"
-#include "inversion.h"
-#include "solarisation.h"
-#include "brightness.h"
-#include "contrast.h"
-#include "gamma_correction.h"
-#include "blur.h"
+#include "parse.h"
 
 int main(int argc, char** argv)
 {
-	//grayscale_test("grass.png");
-	//color_reduction_test("parrot.png");
-	//inversion_test("ball.png");
-	//solarisation_test("sunflower.png");
-	//contrast_test("zebra.png");
-	//brightness_test("pansy.png");
-	//gamma_correction_test("tree.png");
-	//error_diffusion_test("cat.png");
-	blur_test("cballs.png");
-	return 0;
+	return parse();
 }
