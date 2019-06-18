671291ed7eb863a569511ad2f53768f886ec59f3
devmichalek <devmichalek@gmail.com>
2019-06-18 23:19:54 +0200

    Leave execution with -exit command (infinite)

--- Source Files/main.c
+++ Source Files/main.c
@@ -1,6 +1,16 @@
 #include "parse.h"
+#include "semantic.h"
+#include <stdio.h>
 
 int main(int argc, char** argv)
 {
-	return parse();
+	while (!gInterpreter.exitState)
+		parse();
+
+	printf("Press enter to continue...\n");
+	char enter = 0;
+	while (enter != '\r' && enter != '\n')
+		enter = getchar();
+
+	return 0;
 }
